2b9e2618c4bf3027a1141343636f7059e58f2dc5
mryevhenii07 <mrit.works07@gmail.com>
2022-09-05 21:02:25 +0300

    finish row create movies

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,8 +1,9 @@
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-
+import { FaHeart, FaRegHeart } from "react-icons/fa";
 const Row = ({ title, fetchUrl }) => {
   const [movies, setMovies] = useState([]);
+  const [like, setLike] = useState(false);
 
   useEffect(() => {
     axios.get(fetchUrl).then((response) => setMovies(response.data.results));
@@ -15,9 +16,22 @@ const Row = ({ title, fetchUrl }) => {
         {movies.map((item, id) => (
           <div className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2">
             <img
+              className="w-full h-auto block"
               src={`https://image.tmdb.org/t/p/w500/${item?.backdrop_path}`}
               alt={item?.title}
             />
+            <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+              <p className="white-space-normal text-xs md:text-sm fond-bold flex justify-center h-full text-center">
+                {item?.title}
+              </p>
+              <p>
+                {like ? (
+                  <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                ) : (
+                  <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                )}
+              </p>
+            </div>
           </div>
         ))}
       </div>
